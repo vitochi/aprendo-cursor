917b2c09fc55f3a7f971774864d2b92ef0c36a5a
vitochi <vitaro.jmc@gmail.com>
2026-03-17 00:01:27 -0300

    feat: modifico componente Saludo

--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -2,7 +2,7 @@ const Saludo = ({ nombre = 'Programador' }) => {
   return (
     <div>
       <h1>¡Hola, {nombre}!</h1>
-      <p>Tu entorno profesional está listo 🚀</p>
+      <p>Tu entorno profesional está listo para TRABAJAR 🚀</p>
     </div>
   )
 }
